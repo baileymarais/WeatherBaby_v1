01db6e9c19bd2f07694057b9bb2df30f7c6cfcd0
Gavin Masun <maxun@live.cn>
2018-05-02 11:00:18 +0800

    update display demo and sdk

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,7 +1,24 @@
 #define BOARD_TYPE  "arduino uno"
+#define PIN_SDA   8
+#define PIN_SCL   9
+
+#include <Adafruit_ssd1306syp.h>
+Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
 
 void setup() {
+  delay(1000);
+  display.initialize();
 }
 
 void loop() {
+    display.drawLine(0, 0, 127, 63,WHITE);
+    display.update();
+    delay(1000);
+    display.clear();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Connecting WeatherBaby Server...");
+    display.update();
+    display.clear();
 }
